f648da9026547a91cbd4b36cddd93633e5cbaa2a
Voropaev Vitaliy <vvv@anahoret.com>
2016-04-25 14:09:56 +0300

    create new target ANAFacebookDemo
    - add FacebookSDK
    - add scheme ANAFacebookDemo
    - fix structure of folder & groups

--- ANAStream/ANAAppDelegate.m
+++ ANAStream/ANAAppDelegate.m
@@ -8,6 +8,9 @@
 
 #import "ANAAppDelegate.h"
 
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBNotifications/FBNotifications.h>
+
 @interface ANAAppDelegate ()
 
 @end
@@ -16,30 +19,79 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    UIUserNotificationType userNotificationTypes = (UIUserNotificationTypeAlert |
+                                                    UIUserNotificationTypeBadge |
+                                                    UIUserNotificationTypeSound);
+    UIUserNotificationSettings *settings = [UIUserNotificationSettings settingsForTypes:userNotificationTypes
+                                                                             categories:nil];
+    [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+    [[UIApplication sharedApplication] registerForRemoteNotifications];
+    
+    [[FBSDKApplicationDelegate sharedInstance] application:application
+                             didFinishLaunchingWithOptions:launchOptions];
+    
     return YES;
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [FBSDKAppEvents activateApp];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation
+{
+    return [[FBSDKApplicationDelegate sharedInstance] application:application
+                                                          openURL:url
+                                                sourceApplication:sourceApplication
+                                                       annotation:annotation];
+}
+
+#pragma mark -
+#pragma mark Notifications
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    [FBSDKAppEvents setPushNotificationsDeviceToken:deviceToken];
+}
+
+- (void)       application:(UIApplication *)application
+handleActionWithIdentifier:(NSString *)identifier
+     forRemoteNotification:(NSDictionary *)userInfo
+         completionHandler:(void (^)())completionHandler
+{
+    [FBSDKAppEvents logPushNotificationOpen:userInfo action:identifier];
+}
+
+- (void)         application:(UIApplication *)application
+didReceiveRemoteNotification:(NSDictionary *)userInfo
+      fetchCompletionHandler:(nonnull void (^)(UIBackgroundFetchResult))completionHandler
+{
+    [FBSDKAppEvents logPushNotificationOpen:userInfo];
+
+    FBNotificationsManager *notificationsManager = [FBNotificationsManager sharedManager];
+    [notificationsManager presentPushCardForRemoteNotificationPayload:userInfo
+                                                   fromViewController:nil
+                                                           completion:^(FBNCardViewController * _Nullable viewController, NSError * _Nullable error) {
+                                                               if (error) {
+                                                                   completionHandler(UIBackgroundFetchResultFailed);
+                                                               } else {
+                                                                   completionHandler(UIBackgroundFetchResultNewData);
+                                                               }
+                                                           }];
+}
+
 
 @end
